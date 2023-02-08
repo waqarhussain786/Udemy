57ee3cafb73cd59edfe668130524fa219980e090
waqarhussain786 <wh751212@gmail.com>
2023-02-08 13:03:47 +0500

    udemy code

---
+++ functions.js
@@ -0,0 +1,35 @@
+// function declaraction
+function newFuntion() {
+
+}
+
+// function expression
+var newFuntion = function () {
+
+};
+
+//expression
+
+1 + 3;
+var a = 2;
+return true;
+
+//calling or invoking a function
+alert();
+newFuntion(param1, param2);
+
+//assign a variable
+var a = true;
+
+//function vs method
+function thisIsAFunction(params) {
+    
+}
+var obj = {
+    thsiIsAMethod: function (params) {
+        
+    }
+}
+
+thisIsAFunction()
+obj.thsiIsAMethod()
