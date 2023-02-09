8a82fc0850b0ced2a39a9651ddbf6b2c5f63f173
waqarhussain786 <wh751212@gmail.com>
2023-02-09 16:52:54 +0500

    todo function

---
+++ javaLooping.js
@@ -0,0 +1,15 @@
+var todos = [
+    "clean room",
+    "brush teeth",
+    "exercise",
+    "study javaScript",
+    "study javaScript",
+    "study javaScript",
+    "eat healthy",
+    "eat healthy"
+];
+
+
+for (var i = 0; i < todos.length; i++) {
+    console.log(i);
+}
---
+++ todo.js
@@ -0,0 +1,30 @@
+var button = document.getElementById('enter');
+var input = document.getElementById('userinput');
+var ul = document.querySelector('ul');
+
+
+
+function inputLength(){
+    return input.value.length;
+}
+
+function createListElement(){
+    var li = document.createElement('li');
+    li.appendChild(document.createTextNode(input.value));
+    ul.appendChild(li);
+    input.value = "";
+
+}
+
+button.addEventListener("click", function () {
+    if (inputLength() > 0) {
+        createListElement();
+    }
+})
+
+input.addEventListener("keypress", function (event) {
+    if (inputLength() > 0 && event.keyCode === 13) {
+        createListElement();
+    }
+})
+
