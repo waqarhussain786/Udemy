b85834767e5646fb6536ebd09feb6d8e57ff68da
waqarhussain786 <wh751212@gmail.com>
2023-02-23 15:37:57 +0500

    update

--- advance-array.js
+++ advance-array.js
@@ -1,9 +1,30 @@
-const numbers = [4, 5, 6, 7, 8, 9, 0];
+const numbersList = [12, 54, 1, 45, 451, 561, 61, 4131, 513, 6, 146, 16, 51];
 
-const answer = ['This is your answer'];
-
-const a = numbers.forEach((n) => {
-    answer.push(n * 50);    
+const multiplyFunction = numbersList.map((numbers) => {
+    return numbers * 10;
 });
 
-console.log (answer);
+console.log(multiplyFunction);
+
+
+
+
+
+// const numbers = [4, 5, 6, 7, 8, 9, 0];
+
+// const answer = ['This is your answer'];
+
+// const a = numbers.forEach((n) => {
+//     answer.push(n * 50);
+// });
+
+// console.log(answer);
+
+// // _____________________________________________
+
+
+// const mapArray = numbers.map((n) => {
+//     return n * 50;
+// })
+// console.log(mapArray);
+
